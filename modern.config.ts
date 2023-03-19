1272b302555623ad31dbb63e3e52d5d67ab16a23
tivonn <tivon.x@qq.com>
2023-03-19 21:51:28 +0800

    feat: add bff

--- modern.config.ts
+++ modern.config.ts
@@ -1,9 +1,11 @@
 import appTools, { defineConfig } from '@modern-js/app-tools';
+import bffPlugin from '@modern-js/plugin-bff';
+import expressPlugin from '@modern-js/plugin-express';
 
 // https://modernjs.dev/en/configure/app/usage
 export default defineConfig({
   runtime: {
     router: true,
   },
-  plugins: [appTools()],
+  plugins: [appTools(), bffPlugin(), expressPlugin()],
 });
