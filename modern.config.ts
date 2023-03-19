1a2f619e5dc7dddccfff5647f6966ad520df7378
tivonn <tivon.x@qq.com>
2023-03-19 22:15:38 +0800

    feat: add tailwind

--- modern.config.ts
+++ modern.config.ts
@@ -1,11 +1,12 @@
 import appTools, { defineConfig } from '@modern-js/app-tools';
 import bffPlugin from '@modern-js/plugin-bff';
 import expressPlugin from '@modern-js/plugin-express';
+import tailwindCSSPlugin from '@modern-js/plugin-tailwindcss';
 
 // https://modernjs.dev/en/configure/app/usage
 export default defineConfig({
   runtime: {
     router: true,
   },
-  plugins: [appTools(), bffPlugin(), expressPlugin()],
+  plugins: [appTools(), bffPlugin(), expressPlugin(), tailwindCSSPlugin()],
 });
--- src/routes/page.tsx
+++ src/routes/page.tsx
@@ -1,8 +1,16 @@
 import { Helmet } from '@modern-js/runtime/head';
-import './index.css';
 
-const Index = () => (
-  <div className="container-box">
+import 'normalize.css';
+import 'tailwindcss/base.css';
+import 'tailwindcss/components.css';
+import 'tailwindcss/utilities.css';
+import './index.less';
+
+// import Main from './pages/design';
+import { Button } from '@douyinfe/semi-ui';
+
+const Index: React.FC = () => (
+  <div className="app">
     <Helmet>
       <link
         rel="icon"
@@ -10,82 +18,11 @@ const Index = () => (
         href="https://lf3-static.bytednsdoc.com/obj/eden-cn/uhbfnupenuhf/favicon.ico"
       />
     </Helmet>
-    <main>
-      <div className="title">
-        Welcome to
-        <img
-          className="logo"
-          src="https://lf3-static.bytednsdoc.com/obj/eden-cn/zq-uylkvT/ljhwZthlaukjlkulzlp/modern-js-logo.svg"
-          alt="Modern.js Logo"
-        />
-        <p className="name">Modern.js</p>
-      </div>
-      <p className="description">
-        Get started by editing <code className="code">src/routes/page.tsx</code>
-      </p>
-      <div className="grid">
-        <a
-          href="https://modernjs.dev/guides/get-started/introduction.html"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="card"
-        >
-          <h2>
-            Guide
-            <img
-              className="arrow-right"
-              src="https://lf3-static.bytednsdoc.com/obj/eden-cn/zq-uylkvT/ljhwZthlaukjlkulzlp/arrow-right.svg"
-            />
-          </h2>
-          <p>Follow the guides to use all features of Modern.js.</p>
-        </a>
-        <a
-          href="https://modernjs.dev/tutorials/foundations/introduction.html"
-          target="_blank"
-          className="card"
-          rel="noreferrer"
-        >
-          <h2>
-            Tutorials
-            <img
-              className="arrow-right"
-              src="https://lf3-static.bytednsdoc.com/obj/eden-cn/zq-uylkvT/ljhwZthlaukjlkulzlp/arrow-right.svg"
-            />
-          </h2>
-          <p>Learn to use Modern.js to create your first application.</p>
-        </a>
-        <a
-          href="https://modernjs.dev/configure/app/usage.html"
-          target="_blank"
-          className="card"
-          rel="noreferrer"
-        >
-          <h2>
-            Config
-            <img
-              className="arrow-right"
-              src="https://lf3-static.bytednsdoc.com/obj/eden-cn/zq-uylkvT/ljhwZthlaukjlkulzlp/arrow-right.svg"
-            />
-          </h2>
-          <p>Find all configuration items provided by Modern.js.</p>
-        </a>
-        <a
-          href="https://github.com/web-infra-dev/modern.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="card"
-        >
-          <h2>
-            Github
-            <img
-              className="arrow-right"
-              src="https://lf3-static.bytednsdoc.com/obj/eden-cn/zq-uylkvT/ljhwZthlaukjlkulzlp/arrow-right.svg"
-            />
-          </h2>
-          <p>View the source code of Github, feel free to contribute.</p>
-        </a>
-      </div>
-    </main>
+    <div>
+      123
+      <Button>123</Button>
+    </div>
+    {/* <Main /> */}
   </div>
 );
 
